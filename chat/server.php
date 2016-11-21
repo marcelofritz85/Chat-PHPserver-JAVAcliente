9315145a528cb5a6878b783558ce394de40f4e73
Marcelo Fritz <marcelofritz85@gmail.com>
2016-11-21 17:29:09 -0200

    Salvando códigos !

---
+++ chat/server.php
@@ -0,0 +1,33 @@
+<?php
+//	$app_key = "9cc0bc2e04c995ae545e";
+//	$app_secret = "f4d839f39fee5a80bd92";
+//	$app_id = "95063";
+//	$canal = "chat";
+//	$evento = "mensaje";
+        
+        $app_key = "b38bf1a720fdf5255cc0";
+	$app_secret = "af0d007fbafaae565386";
+	$app_id = "272589";
+	$canal = "likely-darkness-895";
+	$evento = "mensaje";
+
+	//importamos la libreria de pusher
+	require('libs/Pusher.php');
+	//comprobamos que llego un mensaje por el metodo GET
+	if($_POST){
+		if(isset($_POST['usuario']) && isset($_POST['mensaje']) && isset($_POST['emoticone'])){
+			//Checamos que los mensages no contengan codigo html ni php a menos que sean emoticones...
+			//Esto es para evitar ataques
+			$usuario = strip_tags($_POST['usuario']);
+			if($_POST['emoticone'] == "false"){
+			 	$mensaje = strip_tags($_POST['mensaje']);
+			}
+			else $mensaje = $_POST['mensaje'];
+
+			/*creamos un objeto pusher que recibe como parametros APP_KEY, APP_SECRET, APP_ID */
+			$pusher = new Pusher($app_key, $app_secret, $app_id);
+			$pusher->trigger($canal, $evento, array('usuario'=> $usuario,'mensaje' => ($mensaje) ));
+			echo json_encode($_POST);
+		}
+	}
+?>
